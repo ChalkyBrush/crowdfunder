b6d7c1d493f9c6e337fcd0064b6247aefa6fbfc3
rraciop <ryanracioppo@gmail.com>
2013-10-01 15:10:37 -0400

    Login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Crowdfunder::Application.routes.draw do
 
   
 
+  get "user_sessions/new"
+
+  get "user_sessions/create"
+
+  get "user_sessions/destroy"
+
   resources :users, :except => [:index]
   resources :projects
+  resources :user_sessions
+
+  	match 'login' => 'user_sessions#new', :as => :login
+	match 'logout' => 'user_sessions#destroy', :as => :logout
 
   root :to => "welcome#index"
 
